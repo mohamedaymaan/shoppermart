e3aa6bb17a9a0e46585dfbae0cb0e3def93c0e5a
mohamedaymaan <mohamd229955@gmail.com>
2025-09-26 22:35:38 +0300

    changeCookies

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -3,8 +3,9 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
+  const TokenSession = (process.env.NODE_ENV === 'production' ? '__Secure-next-auth.session-token' : "next-auth.session-token")
   const cookiesData = await cookies();
-  const encryptToken = cookiesData.get("next-auth.session-token")?.value;
+  const encryptToken = cookiesData.get(TokenSession)?.value;
 
   const data = await decode({
     token: encryptToken,
