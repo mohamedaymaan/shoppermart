04432c47ecd342851a9068da3de0119ce20b2dc4
mohamedaymaan <mohamd229955@gmail.com>
2025-09-27 21:02:45 +0300

    Token

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -3,7 +3,7 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
 export async function getUserToken() {
-  const TokenSession = (process.env.NODE_ENV === 'production' ? '__Secure-next-auth.session-token' : "next-auth.session-token")
+  const TokenSession = (process.env.NODE_ENV === 'production' ? "__Secure-next-auth.session-token" : "next-auth.session-token")
   const cookiesData = await cookies();
   const encryptToken = cookiesData.get(TokenSession)?.value;
 
